d5185816a39a35fb8286329eafdd8dbc1f0491a0
Ivan Eremberg Machado <ivaneremberg@gmail.com>
2022-10-21 14:46:38 -0300

    secao-04-dia-02-exercicio-01

---
+++ modulo-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01/styles.js
@@ -0,0 +1,14 @@
+// Obtenha o valor “Serviços” do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+console.log(menuServices);
+
+// Procure o índice do valor “Portfólio” do array menu1:
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu1.indexOf('Portfólio');
+console.log(indexOfPortfolio);
+
+// Adicione o valor “Contato” no final do array menu2:
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu2.push("Contato");
+console.log(menu2);
