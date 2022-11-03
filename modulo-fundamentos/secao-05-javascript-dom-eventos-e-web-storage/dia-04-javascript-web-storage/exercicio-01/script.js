e2f55fc8e3e51b8867cbb01cb77d3d70fc99721c
Ivan Eremberg Machado <ivaneremberg@gmail.com>
2022-11-03 19:35:42 -0300

    secao-05-dia-04

---
+++ modulo-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/exercicio-01/script.js
@@ -0,0 +1,42 @@
+let articleText = document.querySelector(".inputText");
+
+
+let backgroudColorButtons = document.querySelectorAll(".border .backgroudColor button");
+for (let x of backgroudColorButtons) {
+    x.addEventListener ("click", changeBackgroudColor);
+}
+function changeBackgroudColor (event) {
+    document.body.style.backgroundColor = event.target.innerText;
+}
+
+let fontColorButtons = document.querySelectorAll(".border .fontColor button");
+for (let x of fontColorButtons) {
+    x.addEventListener ("click", changeFontColor);
+}
+function changeFontColor (event) {
+    articleText.style.color = event.target.innerText;
+}
+
+let fontSizeButtons = document.querySelectorAll(".border .fontSize button");
+for (let x of fontSizeButtons) {
+    x.addEventListener ("click", changeFontSize);
+}
+function changeFontSize (event) {
+    articleText.style.fontSize = event.target.innerText;
+}
+
+let lineSpacingButtons = document.querySelectorAll(".border .lineSpacing button");
+for (let x of lineSpacingButtons) {
+    x.addEventListener ("click", changeLineSpacing);
+}
+function changeLineSpacing (event) {
+    articleText.style.lineHeight = event.target.innerText;
+}
+
+let fontTypeButtons = document.querySelectorAll(".border .fontType button");
+for (let x of fontTypeButtons) {
+    x.addEventListener ("click", changeFontType);
+}
+function changeFontType (event) {
+    articleText.style.fontFamily = event.target.innerText;
+}
